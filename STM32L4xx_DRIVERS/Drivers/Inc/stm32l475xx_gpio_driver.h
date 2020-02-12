5263990bf15c5e06dc72a6462c5c06e95bd9612b
Yaoctzin Serrato <syaoctzin@gmail.com>
2020-02-11 23:22:11 -0600

    Added files.
    Added GPIO .c and .h driver files.

---
+++ STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32l475xx_gpio_driver.h
+ *
+ *  Created on: Feb 11, 2020
+ *      Author: yao
+ */
+
+#ifndef INC_STM32L475XX_GPIO_DRIVER_H_
+#define INC_STM32L475XX_GPIO_DRIVER_H_
+
+#include <stm32l475xx.h>
+
+#endif /* INC_STM32L475XX_GPIO_DRIVER_H_ */
---
+++ STM32L4xx_DRIVERS/Drivers/Src/stm32l475xx_gpio_driver.c
@@ -0,0 +1,8 @@
+/*
+ * STM32L475xx_GPIO_DRIV.c
+ *
+ *  Created on: Feb 11, 2020
+ *      Author: yao
+ */
+
+#include <stm32l475xx_gpio_driver.h>
