1c3a0ce146d79ec006f199d312b9dfbba1f53b56
Yaoctzin Serrato <syaoctzin@gmail.com>
2020-02-23 14:41:46 -0600

    Flash latency
    COmpleted the function for configuring the access time to flash according to HCLK value.

--- STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_flash_driver.h
+++ STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_flash_driver.h
@@ -14,12 +14,18 @@
 /* Here go the project includes */
 #include <stm32l475xx.h>
 
+#define	FLASH_LATENCY_ZERO_WAITSTATE		(0)
+#define	FLASH_LATENCY_ONE_WAITSTATE			(1)
+#define	FLASH_LATENCY_TWO_WAITSTATE			(2)
+#define	FLASH_LATENCY_THREE_WAITSTATE		(3)
+#define	FLASH_LATENCY_FOUR_WAITSTATE		(4)
+
 typedef enum
 {
 	FLASH_STATUS_OK = 0,
 	FLASH_STATUS_ERROR = 1
 }FLASH_STATUS;
 
-FLASH_STATUS FLASH_SetLatency(uint32_t freq_HCLK);
+void FLASH_SetLatency(uint32_t freq_HCLK);
 
 #endif /* INC_STM32L475XX_FLASH_DRIVER_H_ */
--- STM32L4xx_DRIVERS/Drivers/Src/stm32l475xx_flash_driver.c
+++ STM32L4xx_DRIVERS/Drivers/Src/stm32l475xx_flash_driver.c
@@ -7,23 +7,50 @@
 
 #include <stm32l475xx_flash_driver.h>
 
-FLASH_STATUS FLASH_SetLatency(uint32_t freq_HCLK)
+void FLASH_SetLatency(uint32_t freq_HCLK)
 {
-	FLASH_STATUS status = FLASH_STATUS_OK;
+	uint32_t LatencyValue = 0;
+
+	/* Clearing register */
+	FLASH->FLASH_ACR &= ~(0x7 << 0);
 
 	/* For voltage Range 1 */
-	//if((freq_HCLK) & (freq_HCLK))
-	//{
+	if((freq_HCLK > 0) & (freq_HCLK <= 16000000UL))
+	{
+		/* 0 wait state */
+		FLASH->FLASH_ACR |= FLASH_LATENCY_ZERO_WAITSTATE;
+		LatencyValue = FLASH_LATENCY_ZERO_WAITSTATE;
+	}
+	else if((freq_HCLK > 16000000UL) & (freq_HCLK <= 32000000UL))
+	{
+		/* 1 wait state */
+		FLASH->FLASH_ACR |= FLASH_LATENCY_ONE_WAITSTATE;
+		LatencyValue = FLASH_LATENCY_ONE_WAITSTATE;
+	}
+	else if((freq_HCLK > 32000000UL) & (freq_HCLK <= 48000000UL))
+	{
+		/* 2 wait state */
+		FLASH->FLASH_ACR |= FLASH_LATENCY_TWO_WAITSTATE;
+		LatencyValue = FLASH_LATENCY_TWO_WAITSTATE;
+	}
+	else if((freq_HCLK > 48000000UL) & (freq_HCLK <= 64000000UL))
+	{
+		/* 3 wait state */
+		FLASH->FLASH_ACR |= FLASH_LATENCY_THREE_WAITSTATE;
+		LatencyValue = FLASH_LATENCY_THREE_WAITSTATE;
+	}
+	else if((freq_HCLK > 64000000UL) & (freq_HCLK <= 80000000UL))
+	{
+		/* 4 wait state */
+		FLASH->FLASH_ACR |= FLASH_LATENCY_FOUR_WAITSTATE;
+		LatencyValue = FLASH_LATENCY_FOUR_WAITSTATE;
+	}
+	else
+	{
+		/* Frequency out of range */
+	}
 
-	//}
-	//else if()
-	//{
+	/* Check if this new setting is being taken into account by reading the LATENCY bits in the FLASH_ACR register */
+	//while((FLASH->FLASH_ACR)&(0x00000007) != LatencyValue);
 
-	//}
-	//else
-	//{
-
-	//}
-
-	return status;
 }
