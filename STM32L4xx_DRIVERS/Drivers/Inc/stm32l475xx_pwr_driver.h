6fce47d219c496e69c7570f5bc0c91a12a09b83c
Rodrigo Serrato <rodrigoyaoctzin.serratoandrade@honeywell.com>
2020-03-14 18:27:12 -0600

    Added Doxygen comments to PWR driver.

--- STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_pwr_driver.h
+++ STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_pwr_driver.h
@@ -1,28 +1,75 @@
-/*
- * stm32l475xx_pwr_driver.h
+/**************************************************************************//**
+ * @file    stm32l475xx_pwr_driver.h
+ * @brief   Header file for stm32l475xx_pwr_driver.c
  *
- *  Created on: Feb 17, 2020
- *      Author: H369169
- */
+ * This file has 1 function declarations (input parameters omitted):
+ *      <br>1) PWR_ControlVoltageScaling()  - Enables the GPIO peripheral clock. </br>
+ *
+ * @version 1.0.0.0
+ *
+ * @author  Yaoctzin Serrato
+ *
+ * @date    24/February/2019
+ ******************************************************************************
+ * @section License
+ * <b> COPYRIGHT (C) 2019 BY: Honeywell International. </b>
+ ******************************************************************************
+ *
+ *
+ *****************************************************************************/
 
+/* Include guard */
 #ifndef INC_STM32L475XX_PWR_DRIVER_H_
 #define INC_STM32L475XX_PWR_DRIVER_H_
 
+/* For C++ */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/******************************************************************************/
+  /* INCLUDES */
+/******************************************************************************/
 /* Here go the system header files */
 #include <stdint.h>
 
 /* Here go the project includes */
 #include <stm32l475xx.h>
 
+/* Here go the own includes */
+
+/*****************************************************************************/
+  /* DEFINES */
+/*****************************************************************************/
+
+/** @name Power voltage ranges.
+ */
+///@{
 #define	PWR_VOLTAGE_RANGE_1		(1U)
 #define	PWR_VOLTAGE_RANGE_2		(2U)
+///@}
 
+/*****************************************************************************/
+  /* TYPEDEFS */
+/*****************************************************************************/
 typedef enum
 {
-	PWR_STATUS_OK,
-	PWR_STATUS_ERROR
+  PWR_STATUS_OK,
+  PWR_STATUS_ERROR
 }PWR_STATUS;
 
+/*****************************************************************************/
+  /* CONSTANTS */
+/*****************************************************************************/
+
+/*****************************************************************************/
+  /* FUNCTION DECLARATIONS */
+/*****************************************************************************/
 PWR_STATUS PWR_ControlVoltageScaling(uint32_t VoltageScaling);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* INC_STM32L475XX_PWR_DRIVER_H_ */
