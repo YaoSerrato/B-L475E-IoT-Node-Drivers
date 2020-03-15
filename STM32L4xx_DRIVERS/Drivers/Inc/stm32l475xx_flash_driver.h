d6b61d282ad226869dfc4ae745d5f1364ff7c871
Rodrigo Serrato <rodrigoyaoctzin.serratoandrade@honeywell.com>
2020-03-14 18:20:21 -0600

    Added doxygen comments to FLASH driver.

--- STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_flash_driver.h
+++ STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_flash_driver.h
@@ -1,31 +1,78 @@
-/*
- * stm32l475xx_flash_driver.h
+/**************************************************************************//**
+ * @file    stm32l475xx_flash_driver.h
+ * @brief   Header file for stm32l475xx_flash_driver.c
  *
- *  Created on: Feb 18, 2020
- *      Author: H369169
- */
+ * This file has 1 function definition (input parameters omitted):
+ *      <br>1) FLASH_SetLatency()  - Enables the GPIO peripheral clock. </br>
+ *
+ * @version 1.0.0.0
+ *
+ * @author  Yaoctzin Serrato
+ *
+ * @date    24/February/2019
+ ******************************************************************************
+ * @section License
+ * <b> COPYRIGHT (C) 2019 BY: Honeywell International. </b>
+ ******************************************************************************
+ *
+ *
+ *****************************************************************************/
 
+/* Include guard */
 #ifndef INC_STM32L475XX_FLASH_DRIVER_H_
 #define INC_STM32L475XX_FLASH_DRIVER_H_
 
+/* For C++ */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/******************************************************************************/
+  /* INCLUDES */
+/******************************************************************************/
 /* Here go the system header files */
 #include <stdint.h>
 
 /* Here go the project includes */
 #include <stm32l475xx.h>
 
+/* Here go the own includes */
+
+/*****************************************************************************/
+  /* DEFINES */
+/*****************************************************************************/
+
+/** @name Flash latency wait states.
+ */
+///@{
 #define	FLASH_LATENCY_ZERO_WAITSTATE		(0)
 #define	FLASH_LATENCY_ONE_WAITSTATE			(1)
 #define	FLASH_LATENCY_TWO_WAITSTATE			(2)
 #define	FLASH_LATENCY_THREE_WAITSTATE		(3)
 #define	FLASH_LATENCY_FOUR_WAITSTATE		(4)
+///@}
 
+/*****************************************************************************/
+  /* TYPEDEFS */
+/*****************************************************************************/
 typedef enum
 {
 	FLASH_STATUS_OK = 0,
 	FLASH_STATUS_ERROR = 1
 }FLASH_STATUS;
 
+/*****************************************************************************/
+  /* CONSTANTS */
+/*****************************************************************************/
+
+/*****************************************************************************/
+  /* FUNCTION DECLARATIONS */
+/*****************************************************************************/
 void FLASH_SetLatency(uint32_t freq_HCLK);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* INC_STM32L475XX_FLASH_DRIVER_H_ */
