f41a93965ffc55e6e6da11caa85442402437cb39
Rodrigo Serrato <rodrigoyaoctzin.serratoandrade@honeywell.com>
2020-03-14 18:16:01 -0600

    Commented out LatencyValue variable

--- STM32L4xx_DRIVERS/Drivers/Src/stm32l475xx_flash_driver.c
+++ STM32L4xx_DRIVERS/Drivers/Src/stm32l475xx_flash_driver.c
@@ -9,7 +9,7 @@
 
 void FLASH_SetLatency(uint32_t freq_HCLK)
 {
-	uint32_t LatencyValue = 0;
+	/* uint32_t LatencyValue = 0; */
 
 	/* Clearing register */
 	FLASH->FLASH_ACR &= ~(0x7 << 0);
@@ -19,31 +19,31 @@ void FLASH_SetLatency(uint32_t freq_HCLK)
 	{
 		/* 0 wait state */
 		FLASH->FLASH_ACR |= FLASH_LATENCY_ZERO_WAITSTATE;
-		LatencyValue = FLASH_LATENCY_ZERO_WAITSTATE;
+		/* LatencyValue = FLASH_LATENCY_ZERO_WAITSTATE; */
 	}
 	else if((freq_HCLK > 16000000UL) & (freq_HCLK <= 32000000UL))
 	{
 		/* 1 wait state */
 		FLASH->FLASH_ACR |= FLASH_LATENCY_ONE_WAITSTATE;
-		LatencyValue = FLASH_LATENCY_ONE_WAITSTATE;
+		/* LatencyValue = FLASH_LATENCY_ONE_WAITSTATE; */
 	}
 	else if((freq_HCLK > 32000000UL) & (freq_HCLK <= 48000000UL))
 	{
 		/* 2 wait state */
 		FLASH->FLASH_ACR |= FLASH_LATENCY_TWO_WAITSTATE;
-		LatencyValue = FLASH_LATENCY_TWO_WAITSTATE;
+		/* LatencyValue = FLASH_LATENCY_TWO_WAITSTATE; */
 	}
 	else if((freq_HCLK > 48000000UL) & (freq_HCLK <= 64000000UL))
 	{
 		/* 3 wait state */
 		FLASH->FLASH_ACR |= FLASH_LATENCY_THREE_WAITSTATE;
-		LatencyValue = FLASH_LATENCY_THREE_WAITSTATE;
+		/* LatencyValue = FLASH_LATENCY_THREE_WAITSTATE; */
 	}
 	else if((freq_HCLK > 64000000UL) & (freq_HCLK <= 80000000UL))
 	{
 		/* 4 wait state */
 		FLASH->FLASH_ACR |= FLASH_LATENCY_FOUR_WAITSTATE;
-		LatencyValue = FLASH_LATENCY_FOUR_WAITSTATE;
+		/* LatencyValue = FLASH_LATENCY_FOUR_WAITSTATE; */
 	}
 	else
 	{
