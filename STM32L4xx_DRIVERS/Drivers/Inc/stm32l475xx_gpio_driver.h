c9f72e90488453262739154f5ccb49293fc993f6
Rodrigo Serrato <rodrigoyaoctzin.serratoandrade@honeywell.com>
2020-04-01 19:32:36 -0600

    Minor changes to GPIO header file (should not be in this branch, by the way).

--- STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_gpio_driver.h
+++ STM32L4xx_DRIVERS/Drivers/Inc/stm32l475xx_gpio_driver.h
@@ -158,13 +158,16 @@ typedef struct  /**< Structure for a GPIO peripheral configuration */
 /*****************************************************************************/
 
 void GPIO_PeriphClkControl(GPIO_RegDef_t* pGPIOx, uint8_t Enabler);
+
 void GPIO_Init(GPIO_Handle_t* pGPIOHandle);
 void GPIO_DeInit(GPIO_RegDef_t* pGPIOx);
+
 uint8_t GPIO_ReadPin(GPIO_RegDef_t* pGPIOx, uint8_t PinNumber);
 uint16_t GPIO_ReadPort(GPIO_RegDef_t* pGPIOx);
 void GPIO_WritePin(GPIO_RegDef_t* pGPIOx, uint8_t PinNumber, uint8_t PinValue);
 void GPIO_WritePort(GPIO_RegDef_t* pGPIOx, uint16_t PortValue);
 void GPIO_TogglePin(GPIO_RegDef_t* pGPIOx, uint8_t PinNumber);
+
 void GPIO_IRQConfig(uint8_t IRQnumber, uint8_t IRQpriority, uint8_t Enabler);
 void GPIO_IRQHandling(uint8_t PinNumber);
 
